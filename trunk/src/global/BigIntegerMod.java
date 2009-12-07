bf376d16188fcfacf9fa2518a05908e4d95fc342
igelka <igelka@3c41d984-dff5-11de-bc38-1b488595cc70>
2009-12-07 21:52:59 +0000

    fixed consts

--- trunk/src/global/BigIntegerMod.java
+++ trunk/src/global/BigIntegerMod.java
@@ -5,16 +5,25 @@ import java.util.Random;
 
 public class BigIntegerMod {
 
-	// TODO - NOTE - use this - globals.GlobalConsts.CERTAINTY
+	// TODO - NOTE - use this - global.Consts.CERTAINTY
 
 	private BigInteger value;
-	private BigInteger modSize;
+	private BigInteger mod;
 
-	public BigIntegerMod(BigInteger modSize, Random random) {
-		// TODO - implement
+	// create a random big integer mod
+	public BigIntegerMod(BigInteger mod) {
+		this.mod = mod;
+		if (this.mod.compareTo(BigInteger.ZERO) <= 0) {
+			throw new IllegalArgumentException("mod <= 0");
+		}
+		do {
+			value = new BigInteger(this.mod.bitLength(), new Random());
+		} while (value.compareTo(this.mod) >= 0);
+
 	}
 
-	public BigIntegerMod(BigInteger modSize, BigInteger value) {
+	// create a big integer mod using the given value
+	public BigIntegerMod(BigInteger value, BigInteger mod) {
 		// TODO - implement
 	}
 
@@ -22,64 +31,59 @@ public class BigIntegerMod {
 		return value;
 	}
 
-	public BigInteger getModSize() {
-		return modSize;
+	public BigInteger getMod() {
+		return mod;
 	}
 
 	public String toString() {
-		return value.toString() + " (mod " + modSize.toString() + ")";
+		return value.toString() + " (mod " + mod.toString() + ")";
 	}
 
-	private void verifyModSize(BigIntegerMod comparator, String op) {
-		verifyModSize(this, comparator, op);
+	private void verifyMod(BigIntegerMod comparator, String op) {
+		verifyMod(this, comparator, op);
 	}
 
-	private static void verifyModSize(BigIntegerMod first, BigIntegerMod second, String op) {
-		// verify that comparator and this have the same modSize
-		if (first.getModSize().compareTo(second.getModSize()) != 0) {
-			throw new ArithmeticException(op + " (BigIntegerMod)s with different (modSize)s");
+	private static void verifyMod(BigIntegerMod first, BigIntegerMod second, String op) {
+		// verify that comparator and this have the same mod
+		if (first.mod.compareTo(second.mod) != 0) {
+			throw new ArithmeticException(op + " (BigIntegerMod)s with different mods");
 		}
 	}
 
 	public BigIntegerMod multiply(BigIntegerMod multiplier) {
-		verifyModSize(multiplier, "multiply");
-		return new BigIntegerMod(modSize, value.multiply(multiplier.value));
+		verifyMod(multiplier, "multiply");
+		return new BigIntegerMod(mod, value.multiply(multiplier.value));
 	}
 
 	public BigIntegerMod add(BigIntegerMod adder) {
-		verifyModSize(adder, "add");
-		return new BigIntegerMod(modSize, value.add(adder.value));
+		verifyMod(adder, "add");
+		return new BigIntegerMod(mod, value.add(adder.value));
 	}
 
 	public BigIntegerMod substract(BigIntegerMod substracter) {
-		verifyModSize(substracter, "substract");
-		return new BigIntegerMod(modSize, value.subtract(substracter.value));
+		verifyMod(substracter, "substract");
+		return new BigIntegerMod(mod, value.subtract(substracter.value));
 	}
 
 	public BigIntegerMod pow(BigInteger exponent) {
-		return new BigIntegerMod(modSize, value.modPow(exponent, modSize));
+		return new BigIntegerMod(mod, value.modPow(exponent, mod));
 	}
 
 	public BigIntegerMod pow(BigIntegerMod exponent) {
-		return new BigIntegerMod(modSize, value.modPow(exponent.value, modSize));
+		return new BigIntegerMod(mod, value.modPow(exponent.value, mod));
 	}
 
 	public BigIntegerMod inverse() {
-		return new BigIntegerMod(modSize, value.modInverse(modSize));
+		return new BigIntegerMod(mod, value.modInverse(mod));
 	}
 
 	public int compareTo(BigIntegerMod comparator) {
-		verifyModSize(comparator, "compare");
+		verifyMod(comparator, "compare");
 		return value.compareTo(comparator.value);
 	}
 
-	static public BigIntegerMod getModFieldGenerator(BigInteger modFieldPrime, int smallPrimeDevisor, BigInteger bigPrimeDevisor) {
-		// TODO - implement
-		return null;
-	}
-
 	static public BigInteger BruteForceDLOG(BigIntegerMod dlogBase, BigIntegerMod dlogResult) {
-		verifyModSize(dlogBase, dlogResult, "DLOG");
+		verifyMod(dlogBase, dlogResult, "DLOG");
 		// TODO - implement
 		return null;
 	}
