2315ab5e7bfcef125327534a5b860f91a1fc1258
yaelkroub@gmail.com <yaelkroub@gmail.com@3c41d984-dff5-11de-bc38-1b488595cc70>
2009-12-19 22:35:15 +0000

    Add performZKP and printToFile functions. Add outputFile and MC_RESULTS_FILE fields.

--- trunk/src/MixCenter/MixCenter.java
+++ trunk/src/MixCenter/MixCenter.java
@@ -1,14 +1,21 @@
 package MixCenter;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 
 import sun.awt.datatransfer.DataTransferer.ReencodingInputStream;
 import elgamal.CryptObject;
 import global.Consts;
+import global.Consts.DebugOutput;
 import elgamal.ElGamal;
 
 public class MixCenter 
 {
+	private static BufferedWriter outputFile = null;
+	private static final String MC_RESULTS_FILE = "Mix_Center_Log.txt";
+	
 	/*
 	 * generates the array (pi) that represents the permutation that will be made.
 	 */
@@ -45,5 +52,69 @@ public class MixCenter
 		}
 		return B;
 	}
+	
+	/*
+	 * Print results to file
+	 * Params: message - string to print
+	 *         A - encrypted votes array before re-encryption and mixing
+	 *         B - re-encrypted and mixed votes array
+	 */
+	public static void printToFile(String message, CryptObject[] A, CryptObject[] B)
+	{
+		int n=Consts.VOTERS_AMOUNT;	//TODO: make it a field so we wont read it all the time?	
+		
+		try 
+		{
+			if (outputFile == null)
+			{
+				outputFile = new BufferedWriter(new FileWriter(MC_RESULTS_FILE));
+			}
+			// print ZKP string
+			outputFile.write(message + "\r\n");
+			
+			// print A array
+			outputFile.write(" A: [ ");
+			for (int i=0; i<n; i++)
+			{
+				outputFile.write(A[i].getText().getValue().toString() + " ");
+			}
+			outputFile.write("]\n");
+			
+			// print B array
+			outputFile.write(" B: [ ");
+			for (int i=0; i<n; i++)
+			{
+				outputFile.write(B[i].getText().getValue().toString() + " ");
+			}
+			outputFile.write("]\n");
+			
+			outputFile.flush(); // TODO: is it possible buffer will be full  sooner?
+		}
+		catch (IOException e) 
+		{
+			System.err.println(e);
+		}	
+	}
+	
+	/*
+	 * Performs Zero Knowledge Proof
+	 * Params: A - encrypted votes array before re-encryption and mixing
+	 *         B - re-encrypted and mixed votes array
+	 *         pi - new permutation array
+	 *         R - random numbers arrays, whichh were used for re-encrypting (according to the original permutation???)
+	 */
+	private void performZKP(CryptObject[] A, CryptObject[] B, int[] pi) // BigIntegerMod[] R)
+	{
+		String sZKP = " ";
+		// get W (the publicKey)
+		
+		/* call ZKP function  
+		sZKP = verifyGIProof(A, B, pi, R, W);*/
+		
+		printToFile(sZKP, A, B);
+				
+	}
+	
+	
 
 }
