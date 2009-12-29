bf3bd3523fe7d49ebba819676a315d79d5c16f2d
alexkertsman <alexkertsman@3c41d984-dff5-11de-bc38-1b488595cc70>
2009-12-29 20:56:51 +0000

    added new class that is sent in socket

---
+++ src/MixCenter/SentObject.java
@@ -0,0 +1,59 @@
+package MixCenter;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+import elgamal.Ciphertext;
+
+import global.BigIntegerMod;
+
+public class SentObject implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Ciphertext[] 	votes_array;
+	private BigIntegerMod 	G;
+	private BigInteger 		P;
+	private BigInteger 		Q;
+	private BigIntegerMod 	W;
+	private	int				N;
+	
+	public SentObject(	Ciphertext[] 	votes_array,
+						BigIntegerMod 	G,
+						BigInteger 		P,
+						BigInteger 		Q,
+						BigIntegerMod 	W,
+						int				N) {
+		this.votes_array = votes_array;
+		this.G = G;
+		this.P = P;
+		this.Q = Q;
+		this.W = W;
+		this.N = N;
+	}
+	
+	public Ciphertext[] get_votes_array(){
+		return votes_array;
+	}
+	
+	public BigIntegerMod get_G(){
+		return G;
+	}
+	
+	public BigInteger get_P(){
+		return P;
+	}
+	
+	public BigInteger get_Q(){
+		return Q;
+	}
+	
+	public BigIntegerMod get_W(){
+		return W;
+	}
+	
+	public int get_N(){
+		return N;
+	}
+}
