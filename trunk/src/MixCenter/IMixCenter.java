3aa8c1adec6298fc5135bda9be383951e4e6e639
alexkertsman <alexkertsman@3c41d984-dff5-11de-bc38-1b488595cc70>
2009-12-29 20:57:55 +0000

    renamed TCP functions

--- trunk/src/MixCenter/IMixCenter.java
+++ trunk/src/MixCenter/IMixCenter.java
@@ -1,8 +1,12 @@
 package MixCenter;
 
+import java.math.BigInteger;
+
 import elgamal.Ciphertext;
 
-public interface IMixCenter 
+import global.BigIntegerMod;
+
+public interface IMixCenter
 {
 	public void generatePermutation();
 	/*
@@ -18,19 +22,43 @@ public interface IMixCenter
 	 */
 	public String performZKP();
 	
-	/*	This function sends array of votes to mix_center.
+	/*	This function sends data to mix_center.
    		The function is blocking.
+   		NOTE: If Mix Center(i+1) doesn't listen, the connection will
+   			  fail immediately and this Mix Center will try to send
+   			  to next available Mix Center(i+2), (i+3)...
     	Parameters: 
-    		votes – array of votes of type vote_class.
+    		votes 	– array of votes of type vote_class.
+    		G, P, Q - Encryption parameters.
+    		W       - Public key.
+    		N		- Number of votes.
    		Returns True if succeeded, else False is returned.*/
-		public boolean send_to_next_mix_center (Ciphertext[ ] votes);
+		public boolean send_to_next_mix_center (Ciphertext[ ] votes,
+												BigIntegerMod G,
+												BigInteger    P,
+												BigInteger	  Q,
+												BigIntegerMod W,
+												int			  N);
 
 	
 	/*	This function receives array of votes from mix_center.
    		The function is blocking.
+   		If nothing received within 1 hour the TCP connection will
+   			be terminated.
+    	Returns array of votes if succeeded, else null is returned. */
+		public Ciphertext[ ] receive_from_prev_mix_center ();
+		
+		
+	/*	This function receives array of votes from mix_center.
+   		The function is blocking.
     	Parameters: 
-    		votes   – array of votes of type vote_class.
-    	Returns True if succeeded, else False is returned. */
-		public boolean receive_basics (Ciphertext[ ] votes);
-
+    		timeout - timeout in MINUTES after which the TCP connection will
+    				  be terminated
+    	Returns array of votes if succeeded, else null is returned */
+		public Ciphertext[ ] receive_from_prev_mix_center (int timeout);
+	
+	/* This function returns number of mix centers that were involved
+	 * during the voting process.
+	 */
+		public int get_num_of_centers_involved();
 }
