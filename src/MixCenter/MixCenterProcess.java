a17c9eec2166a58b2979beda45475da4ecd34652
tomer.heymann <tomer.heymann@3c41d984-dff5-11de-bc38-1b488595cc70>
2010-01-01 17:16:33 +0000

    added checkings on functions return values and log writes according to the return values...

--- src/MixCenter/MixCenterProcess.java
+++ src/MixCenter/MixCenterProcess.java
@@ -39,21 +39,30 @@ public class MixCenterProcess {
 				
 		try{
 			MC = new MixCenter(id);
+			//TODO: is there a need to check that MC isn't null???
 			write("Mix Center No." + id + " is working...");
 			write("Waiting to recieve data from previus MC...");
-			MC.receive_from_prev_mix_center();
-			write("Finished recieving data from previus MC, generating new permutation...");
-			MC.generatePermutation();
-			write("Starting reencypting and permutating the data...");
-			MC.PermutateAndReecncrypt();
-			write("Finished reencypting and permutating the data, performing ZKP...");
-			proof = new String(MC.performZKP());
-			MC.printToFile(proof);
-			write("ZKP is done, proof file was created and has valid data...");
-			write("Starting to send data to the next MC...");
-			MC.send_to_next_mix_center();
-			write("Finished sending the data to the next MC, goodbye :-) ");
-			write("Mix Center No." + id + " is down...\r\n\r\n");
+			if (MC.receive_from_prev_mix_center() != null){
+				write("Finished recieving data from previus MC, generating new permutation...");
+				MC.generatePermutation();
+				write("Starting reencypting and permutating the data...");
+				MC.PermutateAndReecncrypt();
+				write("Finished reencypting and permutating the data, performing ZKP...");
+				proof = new String(MC.performZKP());
+				if (proof != null){
+					MC.printToFile(proof);
+					write("ZKP is done, proof file was created and has valid data...");
+					write("Starting to send data to the next MC...");
+					if (!MC.send_to_next_mix_center())
+						write("Mix Center No." + id + " didn't succeed to send data to one of the next MCs, the elections process failed!!! - goodbye :-( \r\n\r\n");
+					write("Finished sending the data to the next MC, goodbye :-) ");
+					write("Mix Center No." + id + " is down...\r\n\r\n");
+				}
+				else
+					write("Mix Center No." + id + " failed to get valid ZKP, this MC will not take part of the elections... goodbye :-( \r\n\r\n");
+			}
+			else  
+				write("Mix Center No." + id + " didn't succeed to receive data from previus MC, this MC will not take part of the elections... goodbye :-( \r\n\r\n");
 		}catch (IOException e) 
 		{
 			System.err.println(e);
