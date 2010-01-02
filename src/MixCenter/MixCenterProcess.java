cca28143c6111cef5a664835d4048a743351bd85
alexkertsman <alexkertsman@3c41d984-dff5-11de-bc38-1b488595cc70>
2010-01-02 12:00:55 +0000

    handle IOException of write inside the function. Dont let other usres handle it.

--- src/MixCenter/MixCenterProcess.java
+++ src/MixCenter/MixCenterProcess.java
@@ -9,21 +9,25 @@ public class MixCenterProcess {
 
 	protected static String defaultLogFile = "c:\\MClog.txt";
     
-    public static void write(String s) throws IOException {
+    public static void write(String s) {
     write(defaultLogFile, s);
     }
     
-    public static void write(String f, String s) throws IOException {
+    public static void write(String f, String s) {
     TimeZone tz = TimeZone.getTimeZone("GMT+2"); // or PST, MID, etc ...
     Date now = new Date();
     DateFormat df = new SimpleDateFormat ("yyyy.mm.dd hh:mm:ss ");
     df.setTimeZone(tz);
-    String currentTime = df.format(now);
-   
-    FileWriter aWriter = new FileWriter(f, true);
-    aWriter.write(currentTime + ":\r\n" + "-> " + s + "\r\n");
-    aWriter.flush();
-    aWriter.close();
+    String currentTime = df.format(now); 
+    FileWriter aWriter;
+    try {
+    	aWriter = new FileWriter(f, true);
+    	aWriter.write(currentTime + ":\r\n" + "-> " + s + "\r\n");
+    	aWriter.flush();
+    	aWriter.close();
+    } catch (IOException e){ 
+    	System.err.println(e);
+    }
 }
 
 	
@@ -36,37 +40,32 @@ public class MixCenterProcess {
 		MixCenter MC;
 		int id = Integer.parseInt(args[0]);
 		String proof;
-				
-		try{
-			MC = new MixCenter(id);
-			//TODO: is there a need to check that MC isn't null???
-			write("Mix Center No." + id + " is working...");
-			write("Waiting to recieve data from previus MC...");
-			if (MC.receive_from_prev_mix_center() != null){
-				write("Finished recieving data from previus MC, generating new permutation...");
-				MC.generatePermutation();
-				write("Starting reencypting and permutating the data...");
-				MC.PermutateAndReecncrypt();
-				write("Finished reencypting and permutating the data, performing ZKP...");
-				proof = new String(MC.performZKP());
-				if (proof != null){
-					MC.printToFile(proof);
-					write("ZKP is done, proof file was created and has valid data...");
-					write("Starting to send data to the next MC...");
-					if (!MC.send_to_next_mix_center())
-						write("Mix Center No." + id + " didn't succeed to send data to one of the next MCs, the elections process failed!!! - goodbye :-( \r\n\r\n");
-					write("Finished sending the data to the next MC, goodbye :-) ");
-					write("Mix Center No." + id + " is down...\r\n\r\n");
-				}
-				else
-					write("Mix Center No." + id + " failed to get valid ZKP, this MC will not take part of the elections... goodbye :-( \r\n\r\n");
+
+		MC = new MixCenter(id);
+		//TODO: is there a need to check that MC isn't null???
+		write("Mix Center No." + id + " is working...");
+		write("Waiting to recieve data from previus MC...");
+		if (MC.receive_from_prev_mix_center() != null){
+			write("Finished recieving data from previus MC, generating new permutation...");
+			MC.generatePermutation();
+			write("Starting reencypting and permutating the data...");
+			MC.PermutateAndReecncrypt();
+			write("Finished reencypting and permutating the data, performing ZKP...");
+			proof = new String(MC.performZKP());
+			if (proof != null){
+				MC.printToFile(proof);
+				write("ZKP is done, proof file was created and has valid data...");
+				write("Starting to send data to the next MC...");
+				if (!MC.send_to_next_mix_center())
+					write("Mix Center No." + id + " didn't succeed to send data to one of the next MCs, the elections process failed!!! - goodbye :-( \r\n\r\n");
+				write("Finished sending the data to the next MC, goodbye :-) ");
+				write("Mix Center No." + id + " is down...\r\n\r\n");
 			}
-			else  
-				write("Mix Center No." + id + " didn't succeed to receive data from previus MC, this MC will not take part of the elections... goodbye :-( \r\n\r\n");
-		}catch (IOException e) 
-		{
-			System.err.println(e);
+			else
+				write("Mix Center No." + id + " failed to get valid ZKP, this MC will not take part of the elections... goodbye :-( \r\n\r\n");
 		}
+		else  
+			write("Mix Center No." + id + " didn't succeed to receive data from previus MC, this MC will not take part of the elections... goodbye :-( \r\n\r\n");
 
 	}
 
