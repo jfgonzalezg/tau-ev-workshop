9ced0f223615a12767123ac7bcfbbd512aac77f8
alexkertsman <alexkertsman@3c41d984-dff5-11de-bc38-1b488595cc70>
2010-01-03 15:13:51 +0000

    Mix center - make permutate function do nothing (fot testing purposes).              When other modules will be ready it will be returned
    Pret a vote process - new module for testing.
                          As now we dont reencrypt and permutate it just
    					  sends the votes and when it receives it checks that 
    					  got exactly what it sent.

--- src/MixCenter/PretAVoteProcess.java
+++ src/MixCenter/PretAVoteProcess.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 
 import elgamal.Ciphertext;
 import global.BigIntegerMod;
+import global.Consts;
 
 public class PretAVoteProcess {
 
@@ -11,17 +12,19 @@ public class PretAVoteProcess {
 		
 		IMixCenter preta_center = new MixCenter(0);
 		
+		
 		//Create some votes
+		Ciphertext[] votes= new Ciphertext[Consts.VOTERS_AMOUNT];
 		BigIntegerMod g = new BigIntegerMod(new BigInteger("3"),new BigInteger("4"));
 		BigInteger p = new BigInteger("5");
 		BigInteger q = new BigInteger("6");
 		BigIntegerMod w = new BigIntegerMod(new BigInteger("7"),new BigInteger("8"));
-		BigIntegerMod a = new BigIntegerMod(new BigInteger("9"),new BigInteger("10"));
-		BigIntegerMod b = new BigIntegerMod(new BigInteger("11"),new BigInteger("12"));
-		Ciphertext vote = new Ciphertext(a, b);
-		Ciphertext[] votes= new Ciphertext[2];
-		votes[0] = vote;
-		votes[1] = vote;
+		for (int i = 0; i<Consts.VOTERS_AMOUNT; i++){
+			BigIntegerMod a = new BigIntegerMod(new BigInteger("9"+10*i),new BigInteger("10"+10*i));
+			BigIntegerMod b = new BigIntegerMod(new BigInteger("11"+10*i),new BigInteger("12"+10*i));
+			Ciphertext vote = new Ciphertext(a, b);
+			votes[i] = vote;
+		}
 		System.out.println("Start pret a vote");
 		
 		if (preta_center.send_to_next_mix_center(votes, g, p, q, w, votes.length)){
@@ -39,6 +42,17 @@ public class PretAVoteProcess {
 			System.out.println("Got "+A.length+" votes");
 			//Here we can print the votes
 			System.out.println("num_of_centers_involved" + preta_center.get_num_of_centers_involved());
+			
+			//Check that got same values
+			for (int i = 0; i<Consts.VOTERS_AMOUNT; i++){
+				if (A[i].getA().getValue().compareTo(votes[i].getA().getValue()) == 0 && 
+					A[i].getB().getValue().compareTo(votes[i].getB().getValue()) == 0){
+					System.out.println("received data same as from sent i = "+i);
+				} else {
+					System.out.println("ERROR received data different from sent i = "+i);
+				}
+					
+			}
 		}
 		
 		
