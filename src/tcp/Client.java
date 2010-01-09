fc23c5ef63b436c83affa6a06e8cb81258cb49ad
siegel.eli <siegel.eli@gmail.com>
2010-01-09 16:49:35 +0000

    fixed exception print

--- src/tcp/Client.java
+++ src/tcp/Client.java
@@ -62,7 +62,7 @@ public class Client {
 			Consts.log("Client " + connectionNumber + ": timeout reached in receive function, returning null.", Consts.DebugOutput.STDERR);
 			receivedObject = null;
 		} catch (Exception e) {
-			Consts.log("Client " + connectionNumber + ": exception occured in receive(timeout) function, returning null.", Consts.DebugOutput.STDERR);
+			Consts.log("Client " + connectionNumber + ": exception occured in receive(timeout) function, returning null. exeption:"+e.toString(), Consts.DebugOutput.STDERR);
 			receivedObject = null;
 		}
 		return receivedObject;
