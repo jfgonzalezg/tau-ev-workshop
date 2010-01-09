f7d2b840356cb1b307607c3c4071142eabe9d0dc
igelka <igelka@3c41d984-dff5-11de-bc38-1b488595cc70>
2010-01-09 17:26:20 +0000

    removed uneeded toString() calls added sleep betwen initialization of parties, to let the server do each handshake without exceptions

--- src/threshold/TesterThresholdCryptosystem.java
+++ src/threshold/TesterThresholdCryptosystem.java
@@ -13,13 +13,19 @@ public class TesterThresholdCryptosystem {
 		BigInteger p = new BigInteger("23");
 		//BigInteger q = p.subtract(BigInteger.ONE).divide(Consts.TWO);
 		BigIntegerMod g = (new BigIntegerMod(p)).pow(Consts.TWO);
-		System.out.println("g = "+g.toString());
-		int partiesAmount = 1;
+		System.out.println("g = " + g);
+		int partiesAmount = 2;
 		int threshold = 1;
 		ThresholdCryptosystem thresholdCryptosystem = new ThresholdCryptosystem(partiesAmount, threshold, p, g, Consts.THRESHOLD_CENTER_PORT);
 		Party parties[] = new Party[partiesAmount];
 		for (int i=0; i<partiesAmount; ++i) {
 			parties[i] = new Party(i, "localhost", Consts.THRESHOLD_CENTER_PORT);
+			try {
+				// wait between initialization of parties, to let the server do each handshake without errors
+				java.lang.Thread.sleep(2 * Consts.CONNECTION_TIMEOUT);
+			} catch (InterruptedException e) {
+				System.err.println(e);
+			}
 		}
 		
 		System.out.println("waiting the key-exchange protocol to finish");
@@ -37,7 +43,7 @@ public class TesterThresholdCryptosystem {
 			polynom = parties[i].getPrivatePolynom();
 			System.out.print("party "+i+": ");
 			for (int j=0; j<threshold; ++j) {
-				System.out.print(polynom[j].toString()); //TODO print only values
+				System.out.print(polynom[j]); //TODO print only values
 			}
 			System.out.println("");
 		}
@@ -48,7 +54,7 @@ public class TesterThresholdCryptosystem {
 			polynom = parties[i].getPrivatePolynom();
 			System.out.print("party "+i+" public polynom: ");
 			for (int j=0; j<threshold; ++j) {
-				System.out.print(g.pow(polynom[j]).toString()); //TODO print only values
+				System.out.print(g.pow(polynom[j])); //TODO print only values
 			}
 			System.out.println("");
 		}
@@ -57,25 +63,25 @@ public class TesterThresholdCryptosystem {
 		polynom = thresholdCryptosystem.getMutualPolynom();
 		System.out.print("Mutual public polynom: ");
 		for (int j=0; j<threshold; ++j) {
-			System.out.print(polynom[j].toString()); //TODO print only values
+			System.out.print(polynom[j]); //TODO print only values
 		}
 		System.out.println("");
 		
 		// printing the private and public mutual keys
 		System.out.print("private mutual keys:");
 		for (int i=0; i<partiesAmount; ++i) {
-			System.out.print("" + i + ":" + parties[i].getMutualPrivateKey().toString() + " "); //TODO print only values
+			System.out.print("" + i + ":" + parties[i].getMutualPrivateKey() + " "); //TODO print only values
 		}
 		System.out.println("");
 		System.out.print("public mutual keys :");
 		for (int i=0; i<partiesAmount; ++i) {
-			System.out.print("" + i + ":" + g.pow(parties[i].getMutualPrivateKey()).toString() + " "); //TODO print only values
+			System.out.print("" + i + ":" + g.pow(parties[i].getMutualPrivateKey()) + " "); //TODO print only values
 		}
 		System.out.println("");
 		System.out.print("server mutual keys :");
 		polynom = thresholdCryptosystem.getMutualPublicKeys();
 		for (int i=0; i<partiesAmount; ++i) {
-			System.out.print("" + i + ":" + polynom[i].toString() + " "); //TODO print only values
+			System.out.print("" + i + ":" + polynom[i] + " "); //TODO print only values
 		}
 		
 		//TODO finish the sessions
