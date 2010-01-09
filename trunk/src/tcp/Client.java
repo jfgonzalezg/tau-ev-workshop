1067249a154b4d3976f5adb86522de5179117b80
igelka <igelka@3c41d984-dff5-11de-bc38-1b488595cc70>
2010-01-09 00:37:07 +0000

    Added receive() and receive(timeout) functions to the client

--- trunk/src/tcp/Client.java
+++ trunk/src/tcp/Client.java
@@ -22,33 +22,60 @@ public class Client {
 		try {
 			socket = new Socket(hostname, port);
 		} catch (Exception e) {
-			Consts.log("Client: Error connectiong to server:" + e, Consts.DebugOutput.STDERR);
+			Consts.log("Client " + connectionNumber + ": Error connectiong to server:" + e, Consts.DebugOutput.STDERR);
 			return;
 		}
-		Consts.log("Client: Connection accepted " + socket.getInetAddress() + ":" + socket.getPort(), Consts.DebugOutput.STDOUT);
+		Consts.log("Client " + connectionNumber + ": Connection accepted " + socket.getInetAddress() + ":" + socket.getPort(), Consts.DebugOutput.STDOUT);
 
 		/* Creating both Data Stream */
 		try {
 			Sinput = new ObjectInputStream(socket.getInputStream());
 			Soutput = new ObjectOutputStream(socket.getOutputStream());
 		} catch (IOException e) {
-			Consts.log("Client: Exception creating new Input/output Streams: " + e, Consts.DebugOutput.STDERR);
+			Consts.log("Client " + connectionNumber + ": Exception creating new Input/output Streams: " + e, Consts.DebugOutput.STDERR);
 			return;
 		}
 		send(new Integer(connectionNumber)); // predefined handshake
 	}
 
 	public boolean send(Object object) {
-		// send the string to the server
-		Consts.log("Client: Client sending \"" + object + "\" to server", Consts.DebugOutput.STDOUT);
+		// send the object to the server
+		Consts.log("Client " + connectionNumber + ": Client sending \"" + object + "\" to server", Consts.DebugOutput.STDOUT);
 		try {
 			Soutput.writeObject(object);
 			Soutput.flush();
 			return true;
 		} catch (IOException e) {
-			Consts.log("Client: Error writting to the socket: " + e, Consts.DebugOutput.STDERR);
+			Consts.log("Client " + connectionNumber + ": Error writting to the socket: " + e, Consts.DebugOutput.STDERR);
 			return false;
 		}
+	}
+
+	public Object receive() {
+		// receive an object from the server
+		Consts.log("Client " + connectionNumber + ": trying to receive an object from the server", Consts.DebugOutput.STDOUT);
+		Object receivedObject = null;
+		try {
+			receivedObject = Sinput.readObject();
+			Consts.log("Client " + connectionNumber + ": received an object from the server: " + receivedObject, Consts.DebugOutput.STDOUT);
+		} catch (SocketTimeoutException e) {
+			Consts.log("Client " + connectionNumber + ": timeout reached in receive function, returning null.", Consts.DebugOutput.STDERR);
+			receivedObject = null;
+		} catch (Exception e) {
+			Consts.log("Client " + connectionNumber + ": exception occured in receive(timeout) function, returning null.", Consts.DebugOutput.STDERR);
+			receivedObject = null;
+		}
+		return receivedObject;
+	}
+
+	public Object receive(int timeout_miliseconds) {
+		// receive an object from the server with timeout
+		try {
+			socket.setSoTimeout(timeout_miliseconds);
+		} catch (SocketException e) {
+			Consts.log("Client " + connectionNumber + ": exception occured in receive() function, returning null.", Consts.DebugOutput.STDERR);
+		}
+		return receive();
 	}
 
 	public boolean isConnected() {
